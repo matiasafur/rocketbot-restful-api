e95f5484a443892c3a34c1383167e4627c30c636
matias <matias.afur24@gmail.com>
2024-06-10 17:14:26 -0300

    Added Multer instance inside pdfController and also error handling

--- src/controllers/pdfController.js
+++ src/controllers/pdfController.js
@@ -1,7 +1,10 @@
 import { getTextFromPDF, sendEmail } from "../utils/index.js";
+import multer from "multer";
+
+const upload = multer().single('file'); // Helper for file uploads - https://www.npmjs.com/package/multer
 
 const uploadFile = async (req, res) => {
-    try {
+    upload(req, res, async (err) => {
         if (!req.file || !req.body.email) {
             return res.status(400).json({
                 success: false,
@@ -9,20 +12,43 @@ const uploadFile = async (req, res) => {
             });
         }
 
-        const extractedText = await getTextFromPDF(req.file.buffer);
-        await sendEmail(req.body.email, extractedText);
+        // Checks if the file is a PDF
+        if (req.file.mimetype !== 'application/pdf') {
+            return res.status(400).json({
+                success: false,
+                message: 'The uploaded file is not a PDF',
+            });
+        }
 
-        res.json({
-            success: true,
-            message: `The email has been successfully sent to ${req.body.email}`,
-        });
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({
-            success: false,
-            message: 'An error has occurred processing your request',
-        });
-    }
+        // Handling multer errors
+        if (err instanceof multer.MulterError) {
+            return res.status(400).json({
+                success: false,
+                message: err.message,
+            });
+        } else if (err) {
+            return res.status(500).json({
+                success: false,
+                message: 'An error has occurred processing your request'
+            });
+        }
+
+        try {
+            const extractedText = await getTextFromPDF(req.file.buffer);
+            await sendEmail(req.body.email, extractedText);
+
+            res.json({
+                success: true,
+                message: `The email has been successfully sent to ${req.body.email}`,
+            });
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).json({
+                success: false,
+                message: 'An error has occurred processing your request',
+            });
+        }
+    });
 };
 
 export {
