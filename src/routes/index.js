00a89416db36ab82ef3c0d26111bd4bcc6b35f56
matias <matias.afur24@gmail.com>
2024-06-10 17:20:15 -0300

    POST route '/upload' now only receives uploadFile as request handler

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 import express from "express";
-import multer from "multer";
 import { uploadFile } from "../controllers/index.js";
 
 const router = express.Router();
-const upload = multer(); // Helper for file uploads - https://www.npmjs.com/package/multer
 
 // POST route for upload a file
-router.post('/upload', upload.single('file'), uploadFile);
+router.post('/upload', uploadFile);
 
 export default router;
