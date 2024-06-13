378fdf19cbdc02b561bfeb76464d0b222b759daa
matias <matias.afur24@gmail.com>
2024-06-13 18:47:52 -0300

    Fixed misspelled console log message

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import app from "./src/app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is runnig on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
